89819fe51ab19ec88d2fb912f97257ef01905487
smlsunxie <smlsun@gmail.com>
2015-12-16 11:44:45 +0800

    add bootstrap

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,5 +1,3 @@
-let SerialPort = require("serialport").SerialPort;
-
 export default async (cb) => {
 
   let visitorUser = {
@@ -37,24 +35,7 @@ export default async (cb) => {
     let createdVisitor = await models.User.create(visitorUser);
     let createdEditor = await models.User.create(editorUser);
     let createdAdmin = await models.User.create(adminUser);
-
-    console.log('=== process.env.SERIALPORT_OPEN ===', process.env.SERIALPORT_OPEN);
-
-    let SERIALPORT = process.env.SERIALPORT;
-    if(SERIALPORT != undefined){
-      // var RestComm = [128,1,0,0,0,0,0,50,1,0,0,0,0,0,1,0,0,53,1,0]
-
-      var serialPort = new SerialPort(SERIALPORT, {baudrate: 115200}, true);
-      global.serialPort = serialPort;
-
-      let openSerialPort = await new Promise((resolve, reject) => {
-        serialPort.on ('open', function () {
-          resolve(true)
-        });
-      });
-
-      console.log('=== openSerialPort ===', openSerialPort);
-    }
+    await services.hme.connectSerialPort();
 
   } catch (e) {
 
--- server/services/hme.js
+++ server/services/hme.js
@@ -1,11 +1,33 @@
+let SerialPort = require("serialport").SerialPort;
 
 export default class Hme {
+    constructor (serialPortName) {
+      this.serialPortName = serialPortName;
+    }
 
-    constructor (app) {
-      this.hello = (app) => {
-        let hello = 'yes!';
-        return {hello};
+    hello = (app) => {
+      let hello = 'yes!';
+      return {hello};
+    }
+
+    connectSerialPort = async () => {
+      if(this.serialPortName != undefined){
+        // var RestComm = [128,1,0,0,0,0,0,50,1,0,0,0,0,0,1,0,0,53,1,0]
+
+        var serialPort = new SerialPort(this.serialPortName, {baudrate: 115200}, true);
+        this.serialPort = serialPort;
+
+        let openSerialPort = await new Promise((resolve, reject) => {
+          serialPort.on ('open', function () {
+            resolve(true)
+          });
+        });
+
+        console.log('=== openSerialPort ===', openSerialPort);
+      } else {
+        console.log('=== connectSerialPort without serialPortName ===');
       }
+
     }
 
 }
