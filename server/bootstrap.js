ac628d4fde812e6a6f9d7459dc1185d98a49676a
smlsunxie <smlsun@gmail.com>
2015-12-16 11:26:48 +0800

    bootstrap serialport

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,3 +1,5 @@
+let SerialPort = require("serialport").SerialPort;
+
 export default async (cb) => {
 
   let visitorUser = {
@@ -35,6 +37,24 @@ export default async (cb) => {
     let createdVisitor = await models.User.create(visitorUser);
     let createdEditor = await models.User.create(editorUser);
     let createdAdmin = await models.User.create(adminUser);
+
+    console.log('=== process.env.SERIALPORT_OPEN ===', process.env.SERIALPORT_OPEN);
+
+    let SERIALPORT = process.env.SERIALPORT;
+    if(SERIALPORT != undefined){
+      // var RestComm = [128,1,0,0,0,0,0,50,1,0,0,0,0,0,1,0,0,53,1,0]
+
+      var serialPort = new SerialPort(SERIALPORT, {baudrate: 115200}, true);
+      global.serialPort = serialPort;
+
+      let openSerialPort = await new Promise((resolve, reject) => {
+        serialPort.on ('open', function () {
+          resolve(true)
+        });
+      });
+
+      console.log('=== openSerialPort ===', openSerialPort);
+    }
 
   } catch (e) {
 
