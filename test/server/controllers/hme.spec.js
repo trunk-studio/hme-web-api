15855a86b1cb084148202a81d3c8b9ec3b27f447
dmoon <dmoon.t@gmail.com>
2015-12-21 15:54:08 +0800

    searchDevice controller spec

--- test/server/controllers/hme.spec.js
+++ test/server/controllers/hme.spec.js
@@ -21,4 +21,16 @@ describe("hme", () => {
       done(error);
     });
   });
+
+  it.only("SearchDevice", (done) => {
+    let result = request.get("/rest/hme/searchDevice")
+    .expect(200)
+    .end(error, res) => {
+      console.log('res.body', res.body);
+      done(error);
+    });
+    result.body.should.be.Array;
+    result[0].should..have.any.keys('DevID', 'DevGroup');
+  });
+
 });
