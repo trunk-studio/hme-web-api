7c1d297e07d4d413576f96f2c552658763711cce
fuyaode <dan82625@gmail.com>
2015-12-23 18:44:24 +0800

    add chart

--- frontend/js/components/LEDColorPage.js
+++ frontend/js/components/LEDColorPage.js
@@ -11,25 +11,25 @@ const RadioButtonGroup = require('material-ui/lib/radio-button-group');
 const DropDownMenu = require('material-ui/lib/drop-down-menu');
 const RaisedButton = require('material-ui/lib/raised-button');
 
-const Card = require('material-ui/lib/card/card');
-const CardActions = require('material-ui/lib/card/card-actions');
-const CardExpandable = require('material-ui/lib/card/card-expandable');
-const CardHeader = require('material-ui/lib/card/card-header');
-const CardMedia = require('material-ui/lib/card/card-media');
-const CardText = require('material-ui/lib/card/card-text');
-const CardTitle = require('material-ui/lib/card/card-title');
-
-const NVD3Chart = require('../../../public/js/assets/react-nvd3');
+const LineChart = require("react-chartjs").Line;
 
 export default class LEDColorPage extends React.Component {
 
   _cctChanged = (e, value) => {
-    console.log(value,this.refs.colorSlider);
+    console.log(value,this.refs.colorSlider,this.refs.chart);
     this.refs.colorSlider.setState({
       percent: value,
       value: value
     });
+
+    this.refs.chart.state.chart.datasets[0].points[0].value = value * Math.random() * 100;
+    this.refs.chart.state.chart.datasets[0].points[1].value = value * Math.random() * 100;
+    this.refs.chart.state.chart.datasets[0].points[2].value = value * Math.random() * 100;
+    this.refs.chart.state.chart.datasets[0].points[3].value = value * Math.random() * 100;
+    this.refs.chart.state.chart.datasets[0].points[4].value = value * Math.random() * 100;
+    this.refs.chart.state.chart.update();
   }
+
 
   render() {
     let menuItems = [
@@ -39,51 +39,39 @@ export default class LEDColorPage extends React.Component {
        { payload: '4', text: 'Weekends' },
        { payload: '5', text: 'Weekly' },
     ];
-    let datum = [{
-          key: "Cumulative Return",
-          values: [
+    let chartData = {
+        labels: ["", "", "", "", "", "",],
+        datasets: [
             {
-              "label" : "A" ,
-              "value" : -29.765957771107
-            } ,
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
             {
-              "label" : "B" ,
-              "value" : 0
-            } ,
-            {
-              "label" : "C" ,
-              "value" : 32.807804682612
-            } ,
-            {
-              "label" : "D" ,
-              "value" : 196.45946739256
-            } ,
-            {
-              "label" : "E" ,
-              "value" : 0.19434030906893
-            } ,
-            {
-              "label" : "F" ,
-              "value" : -98.079782601442
-            } ,
-            {
-              "label" : "G" ,
-              "value" : -13.925743130903
-            } ,
-            {
-              "label" : "H" ,
-              "value" : -5.1387322875705
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
             }
-          ]
-        }
-      ];
+        ]
+    };
+
     return (
       <Tabs>
         <Tab label="D3">
           <div className="self-center" style={{width: '1200px'}}>
               <div className="col-md-6">
                 <div className="row">
-                  <NVD3Chart id="barChart" type="discreteBarChart" datum={datum} x="label" y="value"/>
+                  <LineChart ref="chart" data={chartData} width="600" height="250"/>
                 </div>
                 <div className="row">
                   <RaisedButton label="全開" />
@@ -123,7 +111,7 @@ export default class LEDColorPage extends React.Component {
                   </div>
                 </div>
                 <div className="row">
-                  <Slider name="CCT" defaultValue={1} description="CCT" onChange={this._cctChanged}/>
+                  <Slider name="CCT" defaultValue={1} description="CCT" step="0.1" onChange={this._cctChanged}/>
                   <Slider name="Bright" defaultValue={1} description="Bright"/>
                 </div>
             </div>
