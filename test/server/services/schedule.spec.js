5cdc4a1bf82deb6692d2b65fe31b6042010f6205
dmoon <dmoon.t@gmail.com>
2015-12-24 16:50:45 +0800

    add schedule spec

--- test/server/services/schedule.spec.js
+++ test/server/services/schedule.spec.js
@@ -1,20 +1,45 @@
-describe("schedule", () => {
+import moment from 'moment';
 
-  it.only("create", async (done) => {
+describe.skip("schedule", () => {
+
+  it("create", async(done) => {
     try {
-      // let result = await* Schedule.findAndCountAll({});
-      // console.log('result',result);
       let newSchedule = {
-        StartDate: '2015/11/10',
+        StartDate: moment('2015/11/10','YYYY/MM/DD'),
         Days: 15
       };
-      let result = await services.schedule.create();
-      result.body[0].should.have.any.keys('StartDate','Days');
+      let result = await services.schedule.create(newSchedule);
+      result.dataValues.should.have.any.keys('StartDate', 'Days');
       done();
 
     } catch (e) {
       done(e);
     }
+
+  });
+
+  describe("query", async done => {
+    before( async done => {
+      try {
+        let newSchedule = {
+          StartDate: moment('1900/11/10','YYYY/MM/DD'),
+          Days: 15
+        };
+        await models.Schedule.create(newSchedule);
+      } catch (e) {
+        done(e);
+      }
+    });
+
+    it( "All" , async done => {
+      try {
+        let result = await services.schedule.findAll();
+        result.toJSON()[0].should.have.any.keys('StartDate', 'Days');
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
 
   });
 
