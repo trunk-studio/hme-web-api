0ccc15c8469c3c9218c858c10d2312d2ba8c75d7
dmoon <dmoon.t@gmail.com>
2015-12-24 15:42:56 +0800

    update default development config

--- server/config/development.default.js
+++ server/config/development.default.js
@@ -2,10 +2,10 @@ export default {
   'port': 3000,
   'domain': 'localhost',
   'connection': {
-    'username': 'root',
-    'password': '123456',
     'database': 'hme',
-    'host': '127.0.0.1',
+    'username': process.env.MYSQL_ENV_MYSQL_USER_NAME || "root",
+    'password': process.env.MYSQL_ENV_MYSQL_ADMIN_PASS || "root",
+    'host': process.env.MYSQL_PORT_3306_TCP_ADDR || "localhost",
     'dialect': 'mysql',
     'force': false
   }
