71468fe8080d92b92f2b85337981f01616ba9f28
dmoon <dmoon.t@gmail.com>
2016-01-05 23:50:47 +0800

    add slider tip

--- frontend/js/actions/ScheduleDetailActions.js
+++ frontend/js/actions/ScheduleDetailActions.js
@@ -1,12 +1,11 @@
 import request from 'axios'
 
 export const RECEIVED_SCHEDULE_DETAIL = 'RECEIVED_SCHEDULE_DETAIL'
+export const RECEIVED_UPDATED_SCHEDULE_DETAIL = 'RECEIVED_UPDATED_SCHEDULE_DETAIL'
 export const SET_SLIDER_VALUE = 'SET_SLIDER_VALUE'
 
 
-
 export function requestGetScheduleDetail(scheduleID) {
-  // dispatch(function() {return {type: REQUEST_LOGIN});
   return (dispatch) => {
     return request
       .get(`/rest/schedule/${scheduleID}`)
@@ -22,9 +21,10 @@ export function receivedScheduleDetail(data) {
   }
 }
 
-export function getSliderValue(schedlueIndex) {
-  return {
-    type: SET_SLIDER_VALUE,
-    index: schedlueIndex
-  }
+export function requestUpdateScheduleDetail(scheduleDetailData) {
+  return (dispatch) => {
+    return request
+      .post('/rest/schedule/update/detail', scheduleDetailData)
+      .then(response => dispatch(receivedScheduleDetail(response.data)));
+  };
 }
