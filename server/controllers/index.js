4c0feb8bdd2e0508e4ea95358a321feacb8d05c1
fuyaode <dan82625@gmail.com>
2016-01-21 11:32:05 +0800

    fix router

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -35,8 +35,6 @@ export default class Routes {
 
           let slaveId =  ctx.params.slaveId;
           let slave = await services.deviceControl.getSlaveHost(slaveId);
-          console.log(slave.id, slave.host,ctx.request.header.host);
-          // slave = slave.dataValues;
           if(slave){
             if(ctx.request.header.host.indexOf(slave.host) != -1){
               console.log("My router");
