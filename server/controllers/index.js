640e77124ec3cc2dca57a9ef10d16457e1375513
fuyaode <dan82625@gmail.com>
2016-01-25 15:07:44 +0800

    add slider bar pic &color

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -112,7 +112,7 @@ export default class Routes {
         <title>HME</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1 ,maximum-scale=1.0, user-scalable=no">
-        <link rel="shortcut icon" href="images/HME.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/public/assets/images/HME.ico" type="image/x-icon" />
         <!--<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet">-->
         </head>
