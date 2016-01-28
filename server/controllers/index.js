f96c7b57ddf46a74cd69802ed3ff870b6630cdfa
fuyaode <dan82625@gmail.com>
2016-01-28 15:55:52 +0800

    add iphone hide browser bar

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -129,6 +129,7 @@ export default class Routes {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>HME</title>
         <meta name="description" content="">
+        <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="viewport" content="width=device-width, initial-scale=1 ,maximum-scale=1.0, user-scalable=no">
         <link rel="shortcut icon" href="/public/assets/images/HME.ico" type="image/x-icon" />
         <!--<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
