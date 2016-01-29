be777d911420e98525f8d1c8fe86fe1c0074ef92
fuyaode <dan82625@gmail.com>
2016-01-29 15:52:40 +0800

    fix getCurrectSetting

--- server/services/schedule.js
+++ server/services/schedule.js
@@ -163,14 +163,14 @@ module.exports = {
           detailFormat.weight = detail.weight;
           let StartTime = detail.StartTime.split(":")
           detailFormat.StartTime = StartTime[0] +":"+ StartTime[1];
-          detailFormat.Config = {};
-          detailFormat.Config.WW = detail.ScheduleDetailConfigs[0].WW;
-          detailFormat.Config.DB = detail.ScheduleDetailConfigs[0].DB;
-          detailFormat.Config.BL = detail.ScheduleDetailConfigs[0].BL;
-          detailFormat.Config.GR = detail.ScheduleDetailConfigs[0].GR;
-          detailFormat.Config.RE = detail.ScheduleDetailConfigs[0].RE;
-          detailFormat.Config.CCT = detail.ScheduleDetailConfigs[0].CCT;
-          detailFormat.Config.Bright = detail.weight * 100;
+          detailFormat.ScheduleDetailConfig = {};
+          detailFormat.ScheduleDetailConfig.WW = detail.ScheduleDetailConfigs[0].WW;
+          detailFormat.ScheduleDetailConfig.DB = detail.ScheduleDetailConfigs[0].DB;
+          detailFormat.ScheduleDetailConfig.BL = detail.ScheduleDetailConfigs[0].BL;
+          detailFormat.ScheduleDetailConfig.GR = detail.ScheduleDetailConfigs[0].GR;
+          detailFormat.ScheduleDetailConfig.RE = detail.ScheduleDetailConfigs[0].RE;
+          detailFormat.ScheduleDetailConfig.CCT = detail.ScheduleDetailConfigs[0].CCT;
+          detailFormat.ScheduleDetailConfig.Bright = detail.weight * 100;
           return detailFormat;
         })
         return format
