771a4c561e6ad20f9acaaf83371d0e818d855a7a
dmoon <dmoon.t@gmail.com>
2016-01-29 15:37:07 +0800

    add middleware

--- frontend/js/containers/app.js
+++ frontend/js/containers/app.js
@@ -5,9 +5,19 @@ require('../../style/css/nv.d3.css');
 require('../../style/css/slider.css');
 
 import React from 'react';
-import { Router, Route, Link } from 'react-router';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Router, Route, Link, IndexRoute } from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+// import { autoRehydrate } from 'redux-persist';
+import createLogger from 'redux-logger';
+import reducers from '../reducers'
+import configureStore from '../store/configureStore';
+const store = configureStore();
+
+const createStoreWithMiddleware = applyMiddleware(thunk, createLogger())(createStore);
+// const store                     = compose(autoRehydrate())(createStoreWithMiddleware)(reducers);
 
 injectTapEventPlugin();
 
@@ -29,11 +39,22 @@ import WifiSetting from '../components/WifiSetting';
 
 
 export default class App extends React.Component {
+
+  _requireAuth = (nextState, replaceState) => {
+    const state = store.getState();
+    console.log('store', state);
+    // if (!state.auth.isLoggedIn) {
+        // const nextPathname = nextState.location.pathname;
+        // replaceState({ nextPathname }, '/login');
+    // }
+  };
+
   render() {
     return (
       <Router>
-        <Route path="/" component={LoginPage} />
-        <Route path="/manage/:tabIndex" component={ManagePage} />
+        <Route path="/" component={ManagePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/manage/:tabIndex" component={ManagePage} onEnter={this._requireAuth}/>
         <Route path="/graph" component={SettingGraph} />
         <Route path="/schedule/list" component={ScheduleList} />
         <Route path="/schedule/edit/:scheduleID" component={ScheduleDetail} />
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link } from 'react-router';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import App from './containers/app';
