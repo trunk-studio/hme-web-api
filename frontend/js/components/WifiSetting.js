17eace137d600ef7a93de7d71e3d49af7384672f
dmoon <dmoon.t@gmail.com>
2016-01-29 14:01:07 +0800

    setup layout mock up

--- frontend/js/components/WifiSetting.js
+++ frontend/js/components/WifiSetting.js
@@ -8,7 +8,7 @@ import {
   Tabs,
   Tab,
   RefreshIndicator,
-  AppBar, IconButton, FlatButton
+  AppBar, IconButton, FlatButton, RadioButton, RadioButtonGroup
 } from 'material-ui'
 
 const NavigationClose = require('material-ui/lib/svg-icons/navigation/close.js');
@@ -18,12 +18,20 @@ export default class  WifiSetting extends React.Component  {
   constructor(props) {
     super(props);
     this.state = {
+      type: 'slave'
     }
   }
 
   componentDidUpdate(prevProps, prevState) {
 
   }
+
+  _handleRadioChanged = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      type: e.target.value
+    });
+  };
 
   render() {
     let roles = [
@@ -32,9 +40,11 @@ export default class  WifiSetting extends React.Component  {
        { payload: 'user', text: '操作人員' }
     ];
     const {loadingStatus} = this.props;
+
+
     return (
-      <div>
-        <AppBar title="Wifi Setting"
+      <div style={{width: '100%', overflowX: 'hidden'}}>
+        <AppBar title="Setup"
           style={{height: '55px', minHeight: '0px', marginTop: '-9px'}}
           titleStyle={{fontSize: '20px'}}
           iconElementLeft={
@@ -43,33 +53,68 @@ export default class  WifiSetting extends React.Component  {
             </IconButton>
           }
         />
-        <div className="self-center" style={{width: "210px"}}>
-          <div className="row">
-            <TextField
-              ref="ssid"
-              floatingLabelText="SSID"
-              hintText="SSID"
-              type="text" />
-          </div>
-          <div className="row">
-            <TextField
-              ref="password"
-              floatingLabelText="Password"
-              hintText="Password"
-              type="password" />
-          </div>
-          <div className="row">
-            <TextField
-              ref="pinCode"
-              floatingLabelText="PIN CODE"
-              hintText="PIN CODE"
-              type="text" />
-          </div>
-          <div className='row'>
-            <RaisedButton label="APPLY" style={{float: 'right'}}/>
-          </div>
+      <div className="row" style={{marginLeft: '25%'}}>
+        <label style={{fontSize: '18px', marginTop: '15px'}}>S/N: HMEPI001</label>
+      </div>
+      <div className="row" style={{marginLeft: '25%'}}>
+        <label style={{fontSize: '18px', marginTop: '15px'}}>Wifi Setting</label>
+      </div>
+      <div className="self-center" style={{width: "210px"}}>
+        <div className="row">
+          <TextField
+            ref="ssid"
+            floatingLabelText="SSID"
+            hintText="SSID"
+            type="text" />
+        </div>
+        <div className="row">
+          <TextField
+            ref="password"
+            floatingLabelText="Password"
+            hintText="Password"
+            type="password" />
+        </div>
+        <div className="row" style={{display: 'none'}}>
+          <TextField
+            ref="pinCode"
+            floatingLabelText="PIN CODE"
+            hintText="PIN CODE"
+            type="text" />
         </div>
       </div>
+      <div className="row" style={{marginLeft: '25%'}}>
+        <label style={{fontSize: '18px', marginTop: '15px'}}>System</label>
+      </div>
+      <div className="self-center" style={{width: "210px"}}>
+        <RadioButtonGroup name="type" defaultSelected="slave" onChange={this._handleRadioChanged}>
+          <RadioButton
+            value="master"
+            label="Master"
+          />
+          <RadioButton
+            value="slave"
+            label="Slave"
+          />
+        </RadioButtonGroup>
+        <TextField
+          style={{ display: (this.state.type == 'master')? 'block' : 'none'}}
+          ref="adminEmail"
+          floatingLabelText="Administrator Email"
+          hintText="Administrator Email"
+          type="text" />
+        <TextField
+          style={{ display: (this.state.type == 'slave')? 'block' : 'none'}}
+          ref="connectToMaster"
+          floatingLabelText="Connect to Master"
+          hintText="Connect to Master"
+          type="text" />
+      </div>
+      <div className="self-center" style={{width: "210px"}}>
+        <div className='row'>
+          <RaisedButton label="APPLY" style={{float: 'right', marginBottom: '15px'}}/>
+        </div>
+      </div>
+    </div>
     );
   }
 }
