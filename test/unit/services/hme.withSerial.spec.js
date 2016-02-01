6d1b8e34a2174a430b30c9e3c86677f2ddb9c845
weberber <s890916@gmail.com>
2016-02-01 15:48:23 +0800

    add spec

--- test/unit/services/hme.withSerial.spec.js
+++ test/unit/services/hme.withSerial.spec.js
@@ -374,8 +374,11 @@ describe("hme with seriel port", () => {
     });
 
     it.only("serial Port getDevState", async done => {
-      // 取得燈具溫度
+      // 取得燈具狀態
+      // devTemp:燈具LED溫度
+      // envTemp:燈具周圍環境溫度
       // data unit: degree centigrade
+      // fanState:風扇狀態，true正常，false異常
       try {
         let devID = 1;
         let result = await services.hme.getDevState(devID);
