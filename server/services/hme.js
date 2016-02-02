a2ea7b2d8398b6365e4c311ec4a40f2eee06d854
weberber <s890916@gmail.com>
2016-02-02 17:03:30 +0800

    add closeFastRun()

--- server/services/hme.js
+++ server/services/hme.js
@@ -18,9 +18,6 @@ export default class Hme {
     return new Promise(r => setTimeout(r, ms));
   };
 
-  hello(){
-    return {msg: 'world'};
-  };
 
 
 
@@ -1011,6 +1008,80 @@ export default class Hme {
     }
   };
 
+  async closeFastRun(devID, groupID)  {
+    try {
+        //rate:0~2000
+        let accDevParams = {
+          u8DevID:devID,
+          groupID:groupID,
+          sFunc:'WordWt',
+          u8DataNum:1,
+          u8Addr_Arry:[61],  //Addr 61 = RTC simulate frequency divider (data: 0~2000 -> 2000Hz~1Hz)
+          u8DataIn_Arry:[(2000 - rate)],
+          u8Mask_Arry:[],
+          RepeatNum:5
+        }
+
+        if(await this.setLedCtrlMode(devID, groupID, 'Normal') == false){
+          console.log('setLedCtrlMode_Error');
+          return (false);
+        }
+
+        let timeTab = [];
+        for (var i = 0; i < 12; i++) {
+          timeTab = [
+            ...timeTab,
+            i * 2,
+            ...[0, 0],
+            ...[0, 0, 0, 0, 0]
+          ];
+        }
+        if(await this.setTimeTab(devID, groupID, timeTab) == false){
+          console.log('setLedCtrlMode_Error');
+          return (false);
+        }
+
+
+        return (result);
+
+    } catch (e) {
+      throw e;
+    }
+  };
+
+
+
+  async getDevState (devID)  {
+    try {
+        let accDevParams = {
+
+          u8DevID:devID,
+          groupID:0,
+          sFunc:'DiscWordRd',
+          u8DataNum:3,
+          u8Addr_Arry:[15, 16 ,30],  //Addr 15 = LED temp, 30 =fan flage
+          u8DataIn_Arry:[],
+          u8Mask_Arry:[],
+          RepeatNum:5
+        }
+
+        console.log('getDevTemp.accDevParams=', accDevParams);
+        let reData = await this.accessDevice(accDevParams);
+        let result = {
+          devTemp: reData.ramData[0] / 100,
+          envTemp: reData.ramData[1] / 100,
+          fanState: (reData.ramData[2] == 0),
+          success: reData.success
+        };
+
+        return (result);
+
+    } catch (e) {
+      throw e;
+    }
+  };
+
+
 
   async accessDevice ({u8DevID, groupID, sFunc, u8DataNum, u8Addr_Arry, u8DataIn_Arry, u8Mask_Arry, RepeatNum})  {
     try {
@@ -1094,6 +1165,7 @@ export default class Hme {
       throw e;
     }
   }
+
 
   _eventsSetup()  {
     let serialPort = this.serialPort;
