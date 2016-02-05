dcad86f66ff5da0120c0100cfbb70956bd837427
dmoon <dmoon.t@gmail.com>
2016-02-05 18:27:00 +0800

    sort timezone

--- frontend/js/components/Setup.js
+++ frontend/js/components/Setup.js
@@ -14,7 +14,7 @@ import {
 const NavigationClose = require('material-ui/lib/svg-icons/navigation/close.js');
 // const timezones = require('../../../timezones.json');
 const timezones = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14 ];
+   -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ];
 export default class Setup extends React.Component  {
 
   constructor(props) {
