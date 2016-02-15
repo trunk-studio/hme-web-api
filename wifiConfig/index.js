69b7554f53e52db09f6299d10c13b23e5bbe76f3
smlsunxie <smlsun@gmail.com>
2016-02-15 13:42:30 +0800

    reset button active only one

--- wifiConfig/index.js
+++ wifiConfig/index.js
@@ -1,9 +1,9 @@
 // button is attaced to pin 17, led to 18
 var GPIO = require('onoff').Gpio,
-    ledA = new GPIO(7, 'out'),
+    ledA = new GPIO(8, 'out'),
     buttonA = new GPIO(20, 'in', 'both');
-    ledB = new GPIO(8, 'out'),
-    buttonB = new GPIO(21, 'in', 'both');
+    // ledB = new GPIO(8, 'out'),
+    // buttonB = new GPIO(21, 'in', 'both');
 
 var exec = require('child_process').exec;
 
@@ -14,15 +14,14 @@ function lightA(err, state) {
   // 1 == pressed, 0 == not pressed
   if(state == 1) {
     // turn LED on
-    
-    var cmd = 'make ap_mode';
+    ledA.writeSync(0);
+
+    var cmd = 'make ap_mode && sudo /sbin/reboot';
 
     exec(cmd, function(error, stdout, stderr) {
       console.log(stdout);
       ledA.writeSync(0);
     });
-
-    ledA.writeSync(0);
 
   } else {
     // turn LED off
@@ -30,28 +29,28 @@ function lightA(err, state) {
   }
 }
 
-function lightB(err, state) {
-  console.log('buttonB click', state);
-  // check the state of the button
-  // 1 == pressed, 0 == not pressed
-  if(state == 1) {
-    // turn LED on
-    var cmd = 'make client_mode';
-
-    exec(cmd, function(error, stdout, stderr) {
-      console.log(stdout);
-      ledB.writeSync(0);
-    });
-    ledB.writeSync(0);
-  } else {
-    // turn LED off
-    ledB.writeSync(1);
-  }
-}
+// function lightB(err, state) {
+//   console.log('buttonB click', state);
+//   // check the state of the button
+//   // 1 == pressed, 0 == not pressed
+//   if(state == 1) {
+//     // turn LED on
+//     var cmd = 'make ap_mode';
+//
+//     exec(cmd, function(error, stdout, stderr) {
+//       console.log(stdout);
+//       ledB.writeSync(0);
+//     });
+//     ledB.writeSync(0);
+//   } else {
+//     // turn LED off
+//     ledB.writeSync(1);
+//   }
+// }
 
 
 // pass the callback function to the
 // as the first argument to watch()
 console.log('ready');
 buttonA.watch(lightA);
-buttonB.watch(lightB);
+// buttonB.watch(lightB);
