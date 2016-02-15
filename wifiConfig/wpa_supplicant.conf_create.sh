e29a901f1b4117866b4933aa66bda33132879e36
smlsunxie <smlsun@gmail.com>
2016-02-15 18:35:27 +0800

    fix wpa config

--- wifiConfig/wpa_supplicant.conf_create.sh
+++ wifiConfig/wpa_supplicant.conf_create.sh
@@ -15,7 +15,7 @@ cat > wpa_supplicant.conf << EOF1
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
 network={
-        ssid=$ssid
-        psk=$password
+        ssid="$ssid"
+        psk="$password"
 }
 EOF1
