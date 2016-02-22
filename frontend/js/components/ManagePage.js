2fc56f404263baee9b57536242401dc3c3cfc34a
dmoon <dmoon.t@gmail.com>
2016-02-22 18:21:16 +0800

    add operator report page

--- frontend/js/components/ManagePage.js
+++ frontend/js/components/ManagePage.js
@@ -437,47 +437,10 @@ export default class ManagePage extends React.Component {
     let email = this.props.reportEmail;
 
     let scheduleList = (
-      <Tab label='Schedule' value='1' key={'scheduleList'} className="tab-item">
+      <Tab label='Schedule' value='2' key={'scheduleList'} className="tab-item">
         <ScheduleList />
       </Tab>
     );
-
-    let reportEmailTab = (
-    <Tab key={'reportEmail'} label="Report" value='2' className="tab-item">
-      <div className="tab-content self-center" >
-        <div className="self-center" style={{width: '500px'}}>
-          <TextField
-            ref="inputReportingEmail"
-            floatingLabelText="Report Email"
-            multiLine={true}
-            value={this.state.tmpEmail}
-            onChange={this._handleEditEmail}
-            type="text" />
-          <RaisedButton onTouchTap={this._saveReportingEmail} label="Save" labelColor="#FFF" backgroundColor="#51A7F9" style={{marginTop:'40px' ,marginLeft:'15px', width: '100px', display: 'inline', position: 'absolute'}}/>
-          <RefreshIndicator
-            size={40}
-            left={10}
-            top={0}
-            status={this.props.loadingEmail}
-            style={{display: 'inline-block',
-              position: 'relative'}} />
-        </div>
-        <div className="self-center" style={{marginTop:'15px',width: '500px'}}>
-          <div style={{width: '500px'}}>
-            <SelectField labelMember="primary" menuItems={reportSlaveList} onChange={this._reportSlaveMenuIndexChanged} ref="setupTestSlaveMenu" style={{width: '250px'}}/>
-            <SelectField labelMember="primary" onChange={this._reportDeviceMenuIndexChanged} ref="setupTestDeviceMenu" menuItems={reportDeviceList} style={{width: '250px', marginLeft: '5px', position: 'absolute'}}/>
-          </div>
-          <div>
-            <div className="row">
-              <h4 className="col-md-2 col-sm-2 col-xs-2" style={{textAlign: 'right'}}>溫度:</h4>
-              <h4 className="col-md-4 col-sm-4 col-xs-4">{this.props.devStatus.devTemp}</h4>
-                <h4 className="col-md-2 col-sm-2 col-xs-2" style={{textAlign: 'right'}}>風扇:</h4>
-                <h4 className="col-md-4 col-sm-4 col-xs-4">{this.props.devStatus.fanState}</h4>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Tab> );
 
     let testingTab = (
     <Tab key={'testingTab'} label="Testing" value='3' className="tab-item">
@@ -505,9 +468,29 @@ export default class ManagePage extends React.Component {
       </div>
     </Tab> );
 
-    let adminFunctionTabs = [];
-    if(this.props.role == 'engineer' || this.props.role == 'administrator')
-      adminFunctionTabs.push(scheduleList, reportEmailTab, testingTab);
+    let adminFunctionTabs = [], reportEmailForm = null;
+    if(this.props.role == 'engineer' || this.props.role == 'administrator') {
+      adminFunctionTabs.push(scheduleList, testingTab);
+      reportEmailForm = (
+       <div className="self-center" style={{width: '500px'}} key={'reportForm'}>
+         <TextField
+           ref="inputReportingEmail"
+           floatingLabelText="Report Email"
+           multiLine={true}
+           value={this.state.tmpEmail}
+           onChange={this._handleEditEmail}
+           type="text" />
+         <RaisedButton onTouchTap={this._saveReportingEmail} label="Save" labelColor="#FFF" backgroundColor="#51A7F9" style={{marginTop:'40px' ,marginLeft:'15px', width: '100px', display: 'inline', position: 'absolute'}}/>
+         <RefreshIndicator
+           size={40}
+           left={10}
+           top={0}
+           status={this.props.loadingEmail}
+           style={{display: 'inline-block',
+             position: 'relative'}} />
+         </div>
+       );
+    }
 
     return (
       <Tabs className="tabs-container" initialSelectedIndex={tabIndex} onChange={this._handleTabChanged} tabItemContainerStyle={{backgroundColor: "#032c70", marginTop: '-15px'}} contentContainerStyle={{backgroundColor: 'rgba(0,0,0,0)'}}>
@@ -553,6 +536,25 @@ export default class ManagePage extends React.Component {
                 <SliderRc ref="CCT" name="CCT" defaultValue={2500} min={2500} max={9000} value={this.state.cctValue} onAfterChange={this._cctChanged} className={this.state.cctSliderStyle+" slider"} />
                 <div style={{marginTop: '-8px'}}>Bright {this.state.brightValue}</div>
                 <SliderRc ref="Bright" name="Bright" value={this.state.brightValue} onAfterChange={this._brightChanged} className="slider" />
+              </div>
+            </div>
+          </div>
+        </Tab>
+        <Tab key={'reportEmail'} label="Report" value='1' className="tab-item">
+          <div className="tab-content self-center" >
+
+            <div className="self-center" style={{marginTop:'15px',width: '500px'}}>
+              <div style={{width: '500px'}}>
+                <SelectField labelMember="primary" menuItems={reportSlaveList} onChange={this._reportSlaveMenuIndexChanged} ref="setupTestSlaveMenu" style={{width: '250px'}}/>
+                <SelectField labelMember="primary" onChange={this._reportDeviceMenuIndexChanged} ref="setupTestDeviceMenu" menuItems={reportDeviceList} style={{width: '250px', marginLeft: '5px', position: 'absolute'}}/>
+              </div>
+              <div>
+                <div className="row">
+                  <h4 className="col-md-2 col-sm-2 col-xs-2" style={{textAlign: 'right'}}>溫度:</h4>
+                  <h4 className="col-md-4 col-sm-4 col-xs-4">{this.props.devStatus.devTemp}</h4>
+                    <h4 className="col-md-2 col-sm-2 col-xs-2" style={{textAlign: 'right'}}>風扇:</h4>
+                    <h4 className="col-md-4 col-sm-4 col-xs-4">{this.props.devStatus.fanState}</h4>
+                </div>
               </div>
             </div>
           </div>
